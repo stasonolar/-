da7c38ccf37ac1262b1990f629011a7361cec36f
OlarStas <olarstas@gmail.com>
2024-10-03 17:53:03 +0300

    привіт

--- класна структури/класна структури.cpp
+++ класна структури/класна структури.cpp
@@ -28,37 +28,43 @@
 //		cout << bday2.day << ' ' << bday2.month << ' ' << bday2.year << ' ' << bday2.yearday << ' ' << bday2.mon_name << endl;
 //}
 
+//#include <iostream>
+//#include <Windows.h>
+//
+//using namespace std;
+//
+//struct site {
+//	float adertise;
+//	float click;
+//	float average;
+//};
+//
+//void inputInfo(site &web) {
+//	cout << "Кіл-сть оголошень / відсоток натисків / середнє натискання на оголошення\n";
+//	cin >> web.adertise >> web.click >> web.average;
+//}
+//
+//void res(site& web) {
+//	float many;
+//	many = web.adertise * (web.click / 100) * web.average;
+//	cout << "Прибуток сайту за день " << many << endl;
+//	cout << "Дані з кіл оголошень / натисків / та середього натискання" << endl;
+//	cout << web.adertise << ' ' << web.click << ' ' << web.average << ' ' << endl;
+//}
+//
+//int main()
+//{
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//	site web;
+//	inputInfo(web);
+//	res(web);
+//	
+//	return 0;
+//}
+
+
 #include <iostream>
 #include <Windows.h>
 
 using namespace std;
-
-struct site {
-	float adertise;
-	float click;
-	float average;
-};
-
-void inputInfo(site &web) {
-	cout << "Кіл-сть оголошень / відсоток натисків / середнє натискання на оголошення\n";
-	cin >> web.adertise >> web.click >> web.average;
-}
-
-void res(site& web) {
-	float many;
-	many = web.adertise * (web.click / 100) * web.average;
-	cout << "Прибуток сайту за день " << many << endl;
-	cout << "Дані з кіл оголошень / натисків / та середього натискання" << endl;
-	cout << web.adertise << ' ' << web.click << ' ' << web.average << ' ' << endl;
-}
-
-int main()
-{
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	site web;
-	inputInfo(web);
-	res(web);
-
-	return 0;
-}
